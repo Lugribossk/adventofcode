446e752a992d3bc77d93730dc809170d7c6e1581
Bo Gotthardt <mail@gotthardt.bo>
2022-12-02 22:04:27 +0100

    2022

---
+++ src/2022/1.ts
@@ -0,0 +1,11 @@
+import {readInput} from "../utils";
+
+const input = readInput(__filename)
+    .split("\r\n\r\n")
+    .map(lines => lines.split("\r\n").map(line => parseInt(line)))
+    .map(nums => nums.reduce((p, c) => p + c, 0))
+    .sort((a, b) => b - a);
+
+console.log(input[0]);
+
+console.log(input[0] + input[1] + input[2]);
---
+++ src/2022/2.ts
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import {readInput} from "../utils";
+
+const input = readInput(__filename).split("\r\n");
+
+const rock = 1;
+const paper = 2;
+const scissors = 3;
+
+const loss = 0;
+const draw = 3;
+const win = 6;
+
+const strategyOwnMove = new Map([
+    ["A X", rock + draw],
+    ["A Y", paper + win],
+    ["A Z", scissors + loss],
+    ["B X", rock + loss],
+    ["B Y", paper + draw],
+    ["B Z", scissors + win],
+    ["C X", rock + win],
+    ["C Y", paper + loss],
+    ["C Z", scissors + draw]
+]);
+
+const strategyResult = new Map([
+    ["A X", scissors + loss],
+    ["A Y", rock + draw],
+    ["A Z", paper + win],
+    ["B X", rock + loss],
+    ["B Y", paper + draw],
+    ["B Z", scissors + win],
+    ["C X", paper + loss],
+    ["C Y", scissors + draw],
+    ["C Z", rock + win]
+]);
+
+console.log(input.map(line => strategyOwnMove.get(line)!).reduce((p, c) => p + c, 0));
+
+console.log(input.map(line => strategyResult.get(line)!).reduce((p, c) => p + c, 0));
