28f7ca2fb61347f74fdce8a28ec422a3c62e42d1
Bo Gotthardt <mail@gotthardt.bo>
2025-10-03 19:51:59 +0200

    Update dependencies

--- src/2024/5.ts
+++ src/2024/5.ts
@@ -1,10 +1,10 @@
 import {readInput} from "../utils";
 
-const [a, b] = readInput(import.meta.url).split("\r\n\r\n\r\n");
+const input = readInput(import.meta.url).split("\r\n\r\n\r\n");
 
-const rules = a.split("\r\n").map(line => {
+const rules = input[0].split("\r\n").map(line => {
     const [before, after] = line.split("|");
     return {before: parseInt(before, 10), after: parseInt(after, 10)};
 });
 
-const updates = b.split("\r\n").map(line => line.split(",").map(n => parseInt(n, 10)));
+const updates = input[1].split("\r\n").map(line => line.split(",").map(n => parseInt(n, 10)));
